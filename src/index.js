8e29b7a03885de631865ed1fb4967451081b3c1b
Sean Leif Hellier <sean@newtropolis.com>
2020-07-22 14:21:15 -0400

    added mapbox code to draw template map

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import mapboxgl from "mapbox-gl";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import mapboxgl from "mapbox-gl";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+mapboxgl.accessToken =
+	"pk.eyJ1Ijoic2VhbmxlaWZoZWxsaWVyIiwiYSI6ImNrNDV5anVzZDBkdjczbHA1dW5rNHNwcmEifQ.iSbpiPjqUBw2n881PqsbuQ";
+
+class Application extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			lng: 5,
+			lat: 34,
+			zoom: 2,
+		};
+	}
+
+	componentDidMount() {
+		const map = new mapboxgl.Map({
+			container: this.mapContainer,
+			style: "mapbox://styles/mapbox/streets-v11",
+			center: [this.state.lng, this.state.lat],
+			zoom: this.state.zoom,
+		});
+
+		map.on("move", () => {
+			this.setState({
+				lng: map.getCenter().lng.toFixed(4),
+				lat: map.getCenter().lat.toFixed(4),
+				zoom: map.getZoom().toFixed(2),
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="sidebarStyle">
+					<div>
+						Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
+						{this.state.zoom}
+					</div>
+				</div>
+				<div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
