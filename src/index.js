855d26fceb7b10b08cf80a32abb51429aa5cb01a
Sean Leif Hellier <sean@newtropolis.com>
2020-07-31 16:39:47 -0400

    added dotenv and a pseudocode text file

--- src/index.js
+++ src/index.js
@@ -7,12 +7,8 @@ import ReactDOM from "react-dom";
 import mapboxgl from "mapbox-gl";
 
 var MAPBOX_ID = process.env.REACT_APP_MAPBOX_KEY;
-// var test_id = process.env.REACT_APP_ENV_TEST;
 
-console.log(MAPBOX_ID);
-
-mapboxgl.accessToken =
-	"pk.eyJ1Ijoic2VhbmxlaWZoZWxsaWVyIiwiYSI6ImNrNDV5anVzZDBkdjczbHA1dW5rNHNwcmEifQ.iSbpiPjqUBw2n881PqsbuQ";
+mapboxgl.accessToken = MAPBOX_ID;
 
 class Application extends React.Component {
 	constructor(props) {
